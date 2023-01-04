9fb663562c96df56c2e92d9cc9fda25aa4c220e9
dacorm <stolen.youth@yandex.ru>
2023-01-05 03:25:58 +0700

    feat: library engine core

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.tsx',
+    entry: './core/nandax.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -25,51 +23,6 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                localIdentName: "[name]__[local]___[hash:base64:5]",
-                            },
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [ 'autoprefixer', {}, ],
-                                ],
-                            },
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|woff|woff2|otf|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1000,
-                            name : 'assets/img/[name].[ext]'
-                        }
-                    }
-                ]
-            }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/public/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        })
-    ]
 };
